60f4d5cb5423732e678900681b1679e44544859e
annisadwiarum <annisadwiarum710@gmail.com>
2024-05-25 10:07:55 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+  return (
+    <main>
+      <h1 className="font-bold text-green-500">GRoqqqqq</h1>
+      <form action="">
+        <input type="text" />
+        <button>kirim</button>
+      </form>
+    </main>
+  )
+}
+
+export default App
